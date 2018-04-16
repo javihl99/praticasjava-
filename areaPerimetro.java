ea3c13f548db3fed0f80652db68d0b7a20a63a41
javier <javihl99@gmail.com>
2018-04-16 23:34:40 +0200

    seguimos probando cosas

--- areaPerimetro.java
+++ areaPerimetro.java
@@ -28,7 +28,7 @@ public class areaPerimetro {
 		area= base * altura;
 		
 		System.out.println("El perimetro del rectangulo es " + perimetro + " .El area es " + area);
-		System.out.println ("esto es una prueba");
+		
 
 		
 	}
